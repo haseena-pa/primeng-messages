5ba941bbee1db85d9b50dcb0ef503d2eaf3a7d7b
haseena-pa <haseenakhader.uk@gmail.com>
2022-11-04 17:31:31 +0000

    PrimeNG Messages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { Message, MessageService } from 'primeng/api';
+import { Message } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [MessageService]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
   messages1: Message[] = [];
-  constructor(private messageService: MessageService) { }
 
 
   ngOnInit(): void {
-    // const htmlContent = `<b>Hello World</b>`;
-    // this.messages1 = [
-    //   { severity: 'success', summary: 'Heading', detail: 'More details....' },
-    //   { severity: 'warn', summary: 'Heading', detail: 'More details....' },
-    //   { severity: 'info', summary: 'Heading', detail: 'More details....' },
-    //   { severity: 'error', summary: 'Heading', detail: htmlContent, icon: 'pi pi-spin pi-spinner' }
-    // ];
-  }
-
-  addMessages() {
-    this.messageService.addAll([
-      { severity: 'success', summary: 'Heading', detail: 'More details....' },
-      { severity: 'warn', summary: 'Heading', detail: 'More details....' }
-    ]);
-  }
-
-  clearMessages() {
-    this.messageService.clear();
+    const htmlContent = `<strong>More details having html</strong>`;
+    this.messages1 = [
+      { severity: 'success', summary: 'Heading', detail: htmlContent }
+    ];
   }
 
 }
