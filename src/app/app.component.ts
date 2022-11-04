6abec3856b0fcd00608b7565c29ed393457c8065
haseena-pa <haseenakhader.uk@gmail.com>
2022-11-04 15:34:51 +0000

    primeng messages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Message, MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [MessageService]
 })
-export class AppComponent {
-  title = 'primeng-messages';
+export class AppComponent implements OnInit {
+  messages1: Message[] = [];
+  constructor(private messageService: MessageService) { }
+
+
+  ngOnInit(): void {
+    // const htmlContent = `<b>Hello World</b>`;
+    // this.messages1 = [
+    //   { severity: 'success', summary: 'Heading', detail: 'More details....' },
+    //   { severity: 'warn', summary: 'Heading', detail: 'More details....' },
+    //   { severity: 'info', summary: 'Heading', detail: 'More details....' },
+    //   { severity: 'error', summary: 'Heading', detail: htmlContent, icon: 'pi pi-spin pi-spinner' }
+    // ];
+  }
+
+  addMessages() {
+    this.messageService.addAll([
+      { severity: 'success', summary: 'Heading', detail: 'More details....' },
+      { severity: 'warn', summary: 'Heading', detail: 'More details....' }
+    ]);
+  }
+
+  clearMessages() {
+    this.messageService.clear();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { MessageModule } from 'primeng/message';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MessageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
